0f44a44c6bc1490a04b721ebffeb3aa9de21bfe3
Adam Robillard <hello@adamrobillard.ca>
2019-11-10 17:35:20 -0500

    Highlight updates

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var dropTop = document.querySelector('.masthead').offsetHeight;
 // Menu Tog
 
 const menuTog = document.getElementById('menu-tog');
+const main = document.querySelector('main');
 const togTop = document.querySelector('.menu-tog .tog-top');
 const togMiddle = document.querySelector('.menu-tog .tog-middle');
 const togBottom = document.querySelector('.menu-tog .tog-bottom');
@@ -12,34 +13,56 @@ const masthead = document.querySelector('.masthead');
 const navSlider = document.querySelector('.nav-slider');
 const btnRSVPheader = document.querySelector('.masthead .btn');
 
+function menuClose() {
+  togTop.style.transform = 'translateY(-50%)';
+  togMiddle.removeAttribute('style');
+  setTimeout(function() {
+    togTop.removeAttribute('style');
+    togBottom.removeAttribute('style');
+  }, 150);
+  menuTog.classList.remove('open');
+  masthead.classList.remove('open');
+  navSlider.classList.remove('open');
+  btnRSVPheader.classList.remove('btn-white');
+}
+
+function menuOpen() {
+  togTop.style.top = '50%';
+  togTop.style.transform = 'translateY(-50%)';
+  togBottom.style.bottom = '50%';
+  togBottom.style.transform = 'translateY(50%)';
+  setTimeout(function() {
+    togTop.style.transform = 'translateY(-50%) rotate(-45deg)';
+    togMiddle.style.transform = 'rotate(45deg)';
+    togBottom.style.transform = 'translateY(50%)';
+    togBottom.style.opacity = '0';
+    masthead.classList.add('open');
+    navSlider.classList.add('open');
+    btnRSVPheader.classList.add('btn-white');
+  }, 150);
+  menuTog.classList.add('open');
+}
+
 menuTog.addEventListener('click', function(e) {
   e.preventDefault();
   if (menuTog.classList.contains('open')) {
-    togTop.style.transform = 'translateY(-50%)';
-    togMiddle.removeAttribute('style');
-    setTimeout(function() {
-      togTop.removeAttribute('style');
-      togBottom.removeAttribute('style');
-    }, 150);
-    this.classList.remove('open');
-    masthead.classList.remove('open');
-    navSlider.classList.remove('open');
-    btnRSVPheader.classList.remove('btn-white');
+    menuClose();
   } else {
-    togTop.style.top = '50%';
-    togTop.style.transform = 'translateY(-50%)';
-    togBottom.style.bottom = '50%';
-    togBottom.style.transform = 'translateY(50%)';
-    setTimeout(function() {
-      togTop.style.transform = 'translateY(-50%) rotate(-45deg)';
-      togMiddle.style.transform = 'rotate(45deg)';
-      togBottom.style.transform = 'translateY(50%)';
-      togBottom.style.opacity = '0';
-      masthead.classList.add('open');
-      navSlider.classList.add('open');
-      btnRSVPheader.classList.add('btn-white');
-    }, 150);
-    this.classList.add('open');
+    menuOpen();
+  }
+});
+
+navSlider.addEventListener('click', function() {
+  if (menuTog.classList.contains('open')) {
+    menuClose();
+  }
+});
+
+window.addEventListener('keydown', function(e) {
+  if (e.keyCode == 27) {
+    if (menuTog.classList.contains('open')) {
+      menuClose();
+    }
   }
 });
 
