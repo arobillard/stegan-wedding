38b4ca83383db09bfb81949f81d48c6f5689ed01
Adam Robillard <hello@adamrobillard.ca>
2019-11-11 09:03:45 -0500

    Added sticky header

--- js/main.js
+++ js/main.js
@@ -82,3 +82,78 @@ sliderNav.style.paddingTop = dropTop + 'px';
 //      element.classList.remove('hovered')
 //    });
 // });
+
+// Sticky Nav
+
+window.addEventListener('scroll', function () {
+  var scrollTop = window.pageYOffset;
+  var heroHeight = document.querySelector('.hero').offsetHeight;
+
+  if (scrollTop > 100) {
+    masthead.classList.add('sticky');
+  } else {
+    masthead.classList.remove('sticky');
+  }
+});
+
+// Smooth Scroll
+
+function scrollTo() {
+	const links = document.querySelectorAll('main a');
+	links.forEach(each => (each.onclick = scrollAnchors));
+}
+
+function scrollAnchors(e, respond = null) {
+	const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+	e.preventDefault();
+	var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+	const targetAnchor = document.querySelector(targetID);
+	if (!targetAnchor) return;
+	const originalTop = distanceToTop(targetAnchor);
+	window.scrollBy({ top: originalTop - dropTop, left: 0, behavior: 'smooth' });
+	const checkIfDone = setInterval(function() {
+		const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+		if (distanceToTop(targetAnchor) === 0 || atBottom) {
+			targetAnchor.tabIndex = '-1';
+			targetAnchor.focus();
+			window.history.pushState('', '', targetID);
+			clearInterval(checkIfDone);
+		}
+	}, 100);
+}
+
+scrollTo();
+
+// Countdown
+
+// Set the date we're counting down to
+    var countDownDate = new Date("Jul 18, 2020 15:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Output the result in an element with id="demo"
+
+      // document.getElementById('cd-days').innerText = days;
+      // document.getElementById('cd-hours').innerText = hours;
+      // document.getElementById('cd-minutes').innerText = minutes;
+      // document.getElementById('cd-seconds').innerText = seconds;
+
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "We're married!";
+      }
+    }, 1);
